8f2cf3c82596c62a800ebc795163cfbfe3b96dc4
rja907 <kmrrajj@gmail.com>
2018-02-06 23:50:11 +0530

    added testing and account fetching

---
+++ test/Inbox.test.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); //constructor
+const web3 = new Web3(ganache.provider()); //instance
+
+//Mocha Structure: Deploy -> Manipulate -> Make an assertion -> repeat
+beforeEach(() => {
+  //Get a list of all accounts.
+  //Almost all web3 functions are asynchronous.
+  web3.eth.getAccounts()
+    .then(fetchedAccounts => {
+      console.log(fetchedAccounts);
+    });
+
+  //Use one of those accounts to deploy the contract!
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', ()=>{
+    
+  })
+})
