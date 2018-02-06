151469b982ccb90808ec5007dd094ec58c5d6413
rja907 <kmrrajj@gmail.com>
2018-02-07 00:00:17 +0530

    added async-await syntax

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,19 +4,24 @@ const Web3 = require('web3'); //constructor
 const web3 = new Web3(ganache.provider()); //instance
 
 //Mocha Structure: Deploy -> Manipulate -> Make an assertion -> repeat
-beforeEach(() => {
+// beforeEach(() => {
+//   //Get a list of all accounts.
+//   //Almost all web3 functions are asynchronous.
+//   web3.eth.getAccounts()
+//     .then(fetchedAccounts => {
+//       console.log(fetchedAccounts);
+//     });
+let accounts;
+beforeEach(async () => {
   //Get a list of all accounts.
   //Almost all web3 functions are asynchronous.
-  web3.eth.getAccounts()
-    .then(fetchedAccounts => {
-      console.log(fetchedAccounts);
-    });
+  accounts = await web3.eth.getAccounts();
 
   //Use one of those accounts to deploy the contract!
 });
 
 describe('Inbox', () => {
   it('deploys a contract', ()=>{
-    
+    console.log(accounts);
   })
 })
