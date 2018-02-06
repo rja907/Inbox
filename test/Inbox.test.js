060754a14aa6f3a619326c87c8584f0570a271fb
rja907 <kmrrajj@gmail.com>
2018-02-07 00:49:36 +0530

    added tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); //constructor
-const web3 = new Web3(ganache.provider()); //instance
+const provider = ganache.provider(); //instance
+const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile');
 
 //Mocha Structure: Deploy -> Manipulate -> Make an assertion -> repeat
@@ -28,10 +29,20 @@ beforeEach(async () => {
     .deploy({ data: bytecode, arguments: ['Hi there!']})
     //instructs web3 to send out a transaction that creates this contract.
     .send({ from: accounts[0], gas: '1000000'});
+  inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
   it('deploys a contract', ()=>{
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0]});
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
   })
 })
